2d1a494e4cf914761150f449b69f0294470c4e11
karimbkb <karimb.kb@gmail.com>
2021-06-24 16:08:48 +0200

    expose api methods in interface

--- src/main/java/de/karimbkb/categoryapi/service/CategoryService.java
+++ src/main/java/de/karimbkb/categoryapi/service/CategoryService.java
@@ -6,7 +6,6 @@ import io.micronaut.context.annotation.Prototype;
 @Prototype
 public class CategoryService {
   public String buildPath(Category c) {
-    // TODO: look into Magentos way of converting names to urls
     return c.getName().toLowerCase().trim().replaceAll(" ", "-");
   }
 }
